ad34852cdc507df4b2b334f55cd4c00219c2a708
Ana Maria <annabertozzi07@gmail.com>
2023-05-04 22:33:48 -0300

    Incluindo Produto

---
+++ src/Servicos/Catalogo/Catalogo.API/Data/CatalogoContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalogo.API.Entidades;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+
+
+namespace Catalogo.API.Data
+{
+    public class CatalogoContext : ICatalogoContext
+    {
+        public CatalogoContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DataBaseSettings:ConnectinString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DataBaseSettings:Database"));
+
+            Produtos = database.GetCollection<Produto>(configuration.GetValue<string>("DataBaseSettings:Collection"));
+
+        }
+
+        public IMongoCollection<Produto> Produtos { get; }
+
+    }
+}
---
+++ src/Servicos/Catalogo/Catalogo.API/Repositorio/IProdutoRepositorycs.cs
@@ -0,0 +1,27 @@
+﻿
+using Catalogo.API.Entidades;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalogo.API.Repositorio
+{
+   public interface IProdutoRepositorycs
+    {
+
+        Task CreateProduto(Produto produto);
+
+        Task<bool> UpdateProduto(Produto produto);
+
+        Task<bool> DeleteProduto(Produto produto);
+
+        Task<IEnumerable<Produto>> GetProduto();
+
+        Task<Produto> GetProduto(string id);
+
+        Task<IEnumerable<Produto>> GetProdutosPorNome(string nome);
+
+        Task<IEnumerable<Produto>> GetProdutosPorCategoria(string categoria);
+
+
+    }
+}
